23d0dc3f47a904816a828332f9f69548839ed907
PatrykZymelka <127216593+PatrykZymelka@users.noreply.github.com>
2023-10-18 10:11:17 +0200

    0.2

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,12 +1,19 @@
 function About() {
-    return(
-        <div className="h-screen w-screen text-white px-20 pt-28 " id="About">
-            <div className="border-l-2 pl-4 border-accent">
-            <p>Hello, I'm</p>
-            <p className="text-primary text-9xl font-bold">Patryk Żymełka</p>
-            <p className="pt-5">Third year <span className="italic ">Computer and Electronics Engineering </span>student at Politechnika Wrocławska, Poland.</p>
-            </div>
-        </div>
-        )
+  return (
+    <div className="h-screen w-screen text-white px-20 pt-28 " id="About">
+      <div className="w-4/5 border-2 pl-4 border-accent h-3/4 overflow-x-auto mr-0 object-contain">
+        <p className="break-all">
+          Hello, I'm
+          <br />
+          <span className="text-transparent bg-clip-text text-5xl pt-2 pb-2 font-bold bg-gradient-to-l from-blue-700 via-sky-300 to-blue-700 bg-300% animate-gradient">
+            Patryk Żymełka
+          </span>
+          <br />
+          Third year Computer and Electronics Engineering student at
+          Politechnika Wrocławska, Poland.
+        </p>
+      </div>
+    </div>
+  );
 }
 export default About;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,45 @@
-const plugin = require('tailwindcss/plugin');
+const plugin = require("tailwindcss/plugin");
 
 export default {
-  content: [
-  "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'text': 'rgb(255, 255, 255)',
-        'background': 'rgb(10, 9, 9)',
-        'primary': 'rgb(1, 81, 198)',
-        'secondary': 'rgb(16, 16, 16)',
-        'accent': 'rgb(0, 123, 192)',
-       },
-       fontSize: {
-        'body-lg': '1rem',
-        'body': '.5rem',
-      }},
+        text: "rgb(255, 255, 255)",
+        background: "rgb(10, 9, 9)",
+        primary: "rgb(1, 81, 198)",
+        secondary: "rgb(16, 16, 16)",
+        accent: "rgb(0, 123, 192)",
+      },
+
+      keyframes: {
+        animatedgradient: {
+          "0%": { backgroundPosition: "0%" },
+          "100%": { backgroundPosition: "300%" },
+        },
+      },
+      backgroundSize: {
+        "300%": "300%",
+      },
+      animation: {
+        gradient: "animatedgradient 8s linear infinite",
+      },
+    },
   },
   plugins: [
-    plugin(function({ addUtilities, addComponents, e, prefix, config }) {
-    const newUtilities = {
-      '.horizontal-tb': {
-        writingMode: 'horizontal-tb',
-      },
-      '.vertical-rl': {
-        writingMode: 'vertical-rl'
-      },
-      '.vertical-lr': {
-        writingMode: 'vertical-lr'
-      }
-    }
-    addUtilities(newUtilities)
-  })],
-}
-
+    plugin(function ({ addUtilities, addComponents, e, prefix, config }) {
+      const newUtilities = {
+        ".horizontal-tb": {
+          writingMode: "horizontal-tb",
+        },
+        ".vertical-rl": {
+          writingMode: "vertical-rl",
+        },
+        ".vertical-lr": {
+          writingMode: "vertical-lr",
+        },
+      };
+      addUtilities(newUtilities);
+    }),
+  ],
+};
