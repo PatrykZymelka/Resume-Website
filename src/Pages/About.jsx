28c07b46a378735aa6a93a47ca4c6ade917b5efb
PatrykZymelka <127216593+PatrykZymelka@users.noreply.github.com>
2026-04-15 06:20:17 +0000

    Updated Projects

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -12,14 +12,14 @@ function About() {
             Patryk Żymełka
           </h1>
           <h1 className="whitespace-pre-line">
-          Embedded Software Engineer
+          Embedded Engineer
         </h1>
       </div>
       <div id="back" className="row-span-2 max-[500px]:mt-5 h-fit  justify-self-center bg-gradient-to-l from-blue-700 via-green-400 to-blue-700 bg-300% hover:animate-fgrad rounded-md w-5/6 z-10 shadow-md duration-1000 hover:scale-[102%] hover:duration-1000 ">
       <div className="innerCard">
         <div className="max-[500px]:overflow-y-scroll bg-gradient-to-l from-blue-100 via-green-100 to-blue-100 text-transparent h-300px hover:bg-gradient-to-l hover:from-blue-400 hover:via-green-100 hover:to-blue-400 bg-clip-text hover:duration-1000 duration-1000">
           <h1 className="max-[1000px]:text-lg font-bold text-5xl min-[500px]:my-10 pb-0 ">About</h1>
-          <h1 className="whitespace-pre-line min-[1200px]:text-2xl mx-24 max-[1200px]:m-4 mb-10 "> I'm a young and creative engineer and developer who loves to program, tinker and draw. Currently I'm studying Electrical and Computer Engineering in english at Politechnika Wrocławska, Poland. My passions lay in front-end development, aplication development, web development and computer networks. My biggest strength is my persistance, I'm alwyas open and eager to learn new things that will help me in the future. I take pride in my work and always strive to do my best. </h1>
+          <h1 className="whitespace-pre-line min-[1200px]:text-2xl mx-24 max-[1200px]:m-4 mb-10 "></h1>
         </div>
       </div>
       </div>
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -4,53 +4,71 @@ import Chat from "../Logos/Chat.png"
 import R from "../Logos/REs.png"
 function Proj() {
     return(
-        <div className="h-fit w-[90%] max-[1200px]:w-[80%] text-white grid  max-[1200px]:grid-cols-2 grid-cols-4 mt-40 min-[1200px]:grid-rows-10 my-10 max-[500px]:gap-x-6 gap-x-10 gap-y-5" id="Project">
-            <div className="col-span-4 max-[1200px]:col-span-2 row-span-1 justify-self-center text-xl shadow-md font-bold z-10">My biggest projects
+        <div className="h-fit w-[90%] max-[1200px]:w-[80%] text-white grid  max-[1200px]:grid-cols-2 grid-cols-2 mt-40 min-[1200px]:grid-rows-10 my-10 max-[500px]:gap-x-6 gap-x-10 gap-y-5" id="Project">
+            <div className="col-span-2 max-[1200px]:col-span-2 row-span-2 justify-self-center text-xl shadow-md font-bold z-10">My Projects:
             </div>
             <div id="back" className="max-[1200px]:order-2 max-[1200px]:col-span-2 col-span-1 row-span-9 max-[500px]:mt-5 h-fit w-fit justify-self-center bg-gradient-to-l from-blue-700 via-green-400 to-blue-700 bg-300% hover:animate-fgrad rounded-md z-10 shadow-md duration-1000 hover:scale-[102%] hover:duration-1000 ">
                 <div className="innerCard3">
                     <div className="grid m-5 gap-5 max-[500px]:text-xs">
-                        <h1 className="whitespace-pre-line text-2xl font-bold ">Java Chat App</h1>
-                        <a href="https://github.com/PatrykZymelka/Java-Chat-Bot">
-                            <img className="border-2  border-primary hover:border-accent rounded-md hover:scale-[101%] duration-500" src={Chat} alt="Chat" />
+                        <h1 className="whitespace-pre-line text-2xl font-bold ">Dice Thrower</h1>
+                        <a href="https://github.com/PatrykZymelka/DiceThrowerFinal">
+                            <img className="border-2  border-primary hover:border-accent rounded-md hover:scale-[101%] duration-500" src="src/Logos/Dice.jpg" alt="Chat" />
                         </a>
-                        <h1 className="whitespace-pre-line">A chat application in which you connect to the desired server port on which you can message other users also connected to it. The app displays not only the messages from other users but also who currently is online.</h1>
-                        <h1 className="whitespace-pre-line">It is a Java application built using sockets, which comes in two parts. The first one is the client app, it contains the GUI and allows users to message each other using it. The second one is the server, which when run enables users to connect to it. While connected, users can see other online people on the same port, allowing them to transfer messages through the server to each other.</h1>
-                        <a href="https://github.com/PatrykZymelka/Java-Chat-Bot" className="w-fit justify-self-center">
+                        <h1 className="whitespace-pre-line">TOODOODODODO</h1>
+                        <h1 className="whitespace-pre-line">TODOOODODODODODO</h1>
+                        <a href="https://github.com/PatrykZymelka/DiceThrowerFinal" className="w-fit justify-self-center">
                             <button className="border-2 border-primary hover:border-accent rounded-md px-12 py-4 hover:scale-[105%] duration-500"><h1 className="whitespace-pre-line text-transparent bg-clip-text font-bold bg-gradient-to-l from-blue-700 via-green-400 to-blue-700 bg-300% hover:animate-fgrad">Github Repositiory</h1></button>
                         </a>
                     </div>
                 </div>
             </div>
-            <div id="back" className="max-[1200px]:order-1 col-span-2 row-span-9 max-[500px]:mt-5 h-fit w-fit  justify-self-center bg-gradient-to-l from-yellow-500 via-yellow-200 to-yellow-500 bg-300% hover:animate-fgrad rounded-md   z-10 shadow-md duration-1000 hover:scale-[102%] hover:duration-1000 ">
+            <div id="back" className="max-[1200px]:order-1 col-span-1 row-span-9 max-[500px]:mt-5 h-fit w-fit  justify-self-center bg-gradient-to-l from-red-500 via-red-200 to-red-500 bg-300% hover:animate-fgrad rounded-md   z-10 shadow-md duration-1000 hover:scale-[102%] hover:duration-1000 ">
                 <div className="innerCard3">
                     <div className="grid m-5 gap-5 max-[500px]:text-xs">
-                        <h1 className="whitespace-pre-line text-3xl font-bold ">Wielka Biblioteka Altdorfu</h1>
-                        <a href="https://patrykzymelka.github.io/biblioteka-altdorfu">
-                            <img className="border-2 hover:border-yellow-600 border-yellow-500 rounded-md hover:scale-[101%] duration-500" src={A} alt="Altdorf" />
+                        <h1 className="whitespace-pre-line text-3xl font-bold ">Stenotype Keyboard</h1>
+                        <a href="https://github.com/PatrykZymelka/StenoKeyboard">
+                            <img className="border-2 hover:border-red-600 border-red-500 rounded-md hover:scale-[101%] duration-500" src="src/Logos/Keyboard.jpg" alt="Altdorf" />
                         </a>
-                        <h1 className="whitespace-pre-line ">My biggest project, thus far. In short it is a compendium of all the rules and helpful articles that are necessary to run a tabletop game of Warhammer Fantasy Roleplay. All of the items have been translated to Polish and organised in a condensed easy to navigate way, in order to simplify the act of looking up any possible problems.</h1>
-                        <h1 className="whitespace-pre-line">This multiple page site has been build using React, without any extenctions. The content in it has been aquired using a python API that scries PDF's and roughly translates them into polish, while organising it into JSON files. Most of the icons have been created by me using vector art. The site is still being regulary updated with more content and the preview is currently hosted on github pages. It is both compatible with desktop and mobile view.</h1>
-                        <a href="https://patrykzymelka.github.io/biblioteka-altdorfu" className="w-fit justify-self-center">
-                            <button className="border-2 border-yellow-500 hover:border-yellow-300 rounded-md px-12 py-4 hover:scale-[105%] duration-500"><h1 className="whitespace-pre-line text-transparent bg-clip-text font-bold bg-gradient-to-l from-yellow-500 via-yellow-100 to-yellow-500 bg-300% animate-fgrad">Preview</h1></button>
+                        <h1 className="whitespace-pre-line ">An Engineering Degree project, focused on building and programming a stenotype keyboard.</h1>
+                        <h1 className="whitespace-pre-line">The project was divided into multiple parts: designing the electronic</h1>
+                        <a href="https://github.com/PatrykZymelka/StenoKeyboard" className="w-fit justify-self-center">
+                            <button className="border-2 border-red-500 hover:border-red-400 rounded-md px-12 py-4 hover:scale-[105%] duration-500"><h1 className="whitespace-pre-line text-transparent bg-clip-text font-bold bg-gradient-to-l from-red-500 via-red-100 to-red-500 bg-300% animate-fgrad">Github Repository</h1></button>
                         </a>
                     </div>
                 </div>
             </div>
-            <div id="back" className="max-[1200px]:order-3 max-[1200px]:col-span-2 col-span-1 row-span-9 max-[500px]:mt-5 h-fit w-fit justify-self-center bg-gradient-to-l from-blue-700 via-green-400 to-blue-700 bg-300% hover:animate-fgrad rounded-md z-10 shadow-md duration-1000 hover:scale-[102%] hover:duration-1000 ">
+            <div id="back" className="max-[1200px]:order-2 max-[1200px]:col-span-2 col-span-1 row-span-9 max-[500px]:mt-5 h-fit w-fit justify-self-center bg-gradient-to-l from-blue-700 via-green-400 to-blue-700 bg-300% hover:animate-fgrad rounded-md z-10 shadow-md duration-1000 hover:scale-[102%] hover:duration-1000 ">
                 <div className="innerCard3">
                     <div className="grid m-5 gap-5 max-[500px]:text-xs">
-                        <h1 className="whitespace-pre-line text-2xl font-bold">Frequency Counter</h1>
-                        <a >
-                            <img className="border-2  border-primary hover:border-accent rounded-md hover:scale-[101%] duration-500" src={R} alt="Resume" />
+                        <h1 className="whitespace-pre-line text-2xl font-bold ">Arduino Leonardo Shield</h1>
+                        <a href="https://github.com/PatrykZymelka/Arduino-Leonardo-Shield">
+                            <img className="border-2  border-primary hover:border-accent rounded-md hover:scale-[101%] duration-500" src="src/Logos/Shield.jpg" alt="Chat" />
                         </a>
-                        <h1 className="whitespace-pre-line">Personal resume site on which you are currently on. It was built in order to showcase my frontend skills, tell you more about myself and give you my contact information.</h1>
-                        <h1 className="whitespace-pre-line">https://www.linkedin.com/posts/kaczmarek-grzegorz_synchronization-timekeeping-frequency-activity-7214203512904163328-ULe9?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEHUzWwBQasXKeeuTz5Rp4o7L23ioTOB4fo</h1>
-                        <a  className="w-fit justify-self-center">
-                            </a>
+                        <h1 className="whitespace-pre-line">TOODODODODODOD</h1>
+                        <h1 className="whitespace-pre-line">TODOODODODDOODODOD</h1>
+                        <a href="https://github.com/PatrykZymelka/Arduino-Leonardo-Shield" className="w-fit justify-self-center">
+                            <button className="border-2 border-primary hover:border-accent rounded-md px-12 py-4 hover:scale-[105%] duration-500"><h1 className="whitespace-pre-line text-transparent bg-clip-text font-bold bg-gradient-to-l from-blue-700 via-green-400 to-blue-700 bg-300% hover:animate-fgrad">Github Repositiory</h1></button>
+                        </a>
                     </div>
                 </div>
             </div>
+            <div id="back" className="max-[1200px]:order-2 max-[1200px]:col-span-2 col-span-1 row-span-9 max-[500px]:mt-5 h-fit w-fit justify-self-center bg-gradient-to-l from-blue-700 via-green-400 to-blue-700 bg-300% hover:animate-fgrad rounded-md z-10 shadow-md duration-1000 hover:scale-[102%] hover:duration-1000 ">
+                <div className="innerCard3">
+                    <div className="grid m-5 gap-5 max-[500px]:text-xs">
+                        <h1 className="whitespace-pre-line text-2xl font-bold ">Frequency Counter</h1>
+                        <a href="https://www.linkedin.com/posts/kaczmarek-grzegorz_synchronization-timekeeping-frequency-activity-7214203512904163328-ULe9">
+                            <img className="border-2  border-primary hover:border-accent rounded-md hover:scale-[101%] duration-500" src="src/Logos/KPZ.png" alt="Chat" />
+                        </a>
+                        <h1 className="whitespace-pre-line">Project made for, and with the assistance of, Thaumatec Tech Group. This device was created to precisely measure signals up to 10MHz. With such high frequencies normal results would drift by couple of Hertz, due to internal heating or task scheaduling. While our device by using GNSS receiver as a time reference was able to acquire precise results up to the resolution of 0.1Hz.</h1>
+                        <h1 className="whitespace-pre-line">The device was </h1>
+                        <a href="https://www.linkedin.com/posts/kaczmarek-grzegorz_synchronization-timekeeping-frequency-activity-7214203512904163328-ULe9" className="w-fit justify-self-center">
+                            <button className="border-2 border-primary hover:border-accent rounded-md px-12 py-4 hover:scale-[105%] duration-500"><h1 className="whitespace-pre-line text-transparent bg-clip-text font-bold bg-gradient-to-l from-blue-700 via-green-400 to-blue-700 bg-300% hover:animate-fgrad">Story on Linkedin from the Supervisor</h1></button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            
+            
         </div>
         )
 }
